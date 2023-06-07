223897c7dd6b91de889c531d2ed865df4208bb2f
sonofmom <sonofmom@localhost>
2023-06-07 20:17:23 +0200

    Add SHASUM generation for dumps + adjust validator service

--- node/snapshots/ton_mkdump.sh
+++ node/snapshots/ton_mkdump.sh
@@ -30,7 +30,8 @@ ARCHIVE_LIFETIME=2
 TON_ZFS_FS="$POOL/$DS_NAME"
 TON_ZFS_MOUNTPOINT="/$POOL/$DS_NAME"
 ARCHIVE_PREFIX="ton_dump"
-ARCHIVE_FILENAME="$ARCHIVE_PREFIX$NAME_SUFFIX.`date +"%d_%m_%Y.%H-%M-%S"`.$DUMP_TYPE.lz"
+ARCHIVE_NAME="$ARCHIVE_PREFIX$NAME_SUFFIX.`date +"%d_%m_%Y.%H-%M-%S"`.$DUMP_TYPE"
+ARCHIVE_FILENAME="$ARCHIVE_NAME.lz"
 
 TAR_BIN="/usr/bin/tar"
 PV_BIN="/usr/bin/pv"
@@ -44,6 +45,7 @@ STAT_BIN="/usr/bin/stat"
 ZFS_BIN="/usr/sbin/zfs"
 XARGS_BIN="/usr/bin/xargs"
 GREP_BIN="/usr/bin/grep"
+SHASUM_BIN="/usr/bin/shasum"
 
 check_errs()
 {
@@ -73,7 +75,7 @@ $RM_BIN -R $TON_ZFS_MOUNTPOINT/db/config.json* $TON_ZFS_MOUNTPOINT/db/keyring $T
 cd $TON_ZFS_MOUNTPOINT && $LS_BIN -tp | $GREP_BIN -v db\/ | $XARGS_BIN -I {} $RM_BIN -R -- {}
 
 echo "Cleansing workpath"
-$RM_BIN $WORK_PATH/$ARCHIVE_PREFIX$NAME_SUFFIX* >/dev/null 2>&1
+$RM_BIN $WORK_PATH/$ARCHIVE_PREFIX$NAME_SUFFIX.* >/dev/null 2>&1
 
 if [ "${DUMP_TYPE}" == "zfs" ]
 then
@@ -82,27 +84,33 @@ then
 	$ZFS_BIN snapshot $TON_ZFS_FS@dumpstate >/dev/null 2>&1
 
 	echo "Creating ZFS archive, this will take some time!"
-	$ZFS_BIN send $TON_ZFS_FS@dumpstate | $PV_BIN | $PLZIP_BIN $PLZIP_PARAMS > $WORK_PATH/$ARCHIVE_FILENAME
+	$ZFS_BIN send $TON_ZFS_FS@dumpstate | $PV_BIN | $PLZIP_BIN $PLZIP_PARAMS > $WORK_PATH/$ARCHIVE_NAME.lz
 else
 	echo "Creating TAR archive, this will take some time!"
 	cd $TON_ZFS_MOUNTPOINT/db
-	$TAR_BIN -c * | $PV_BIN | $PLZIP_BIN $PLZIP_PARAMS > $WORK_PATH/$ARCHIVE_FILENAME
+	$TAR_BIN -c * | $PV_BIN | $PLZIP_BIN $PLZIP_PARAMS > $WORK_PATH/$ARCHIVE_NAME.lz
 fi
 
-echo "Moving archive into dumps path"
-$MV_BIN $WORK_PATH/$ARCHIVE_FILENAME $TARGET_PATH
+echo "Creating SHA256 checksum of archive"
+cd $WORK_PATH && $SHASUM_BIN -a 256 $ARCHIVE_NAME.lz > $ARCHIVE_NAME.sha256sum.txt
+
+echo "Creating archive size file"
+$STAT_BIN -c%s $WORK_PATH/$ARCHIVE_NAME.lz > $WORK_PATH/$ARCHIVE_NAME.size.archive.txt
+
+echo "Creating DB size file"
+$ZFS_BIN get -p -H -o value logicalused $TON_ZFS_FS > $WORK_PATH/$ARCHIVE_NAME.size.disk.txt
+
+echo "Moving files into dumps path"
+$MV_BIN $WORK_PATH/$ARCHIVE_PREFIX$NAME_SUFFIX.* $TARGET_PATH
 
 echo "Linking as latest"
-$RM_BIN $TARGET_PATH/latest$NAME_SUFFIX.$DUMP_TYPE.lz
-$LN_BIN -s $TARGET_PATH/$ARCHIVE_FILENAME $TARGET_PATH/latest$NAME_SUFFIX.$DUMP_TYPE.lz
-
-echo "Updating archive size file"
-$STAT_BIN -c%s $TARGET_PATH/$ARCHIVE_FILENAME > $TARGET_PATH/latest$NAME_SUFFIX.size.archive.txt
-
-echo "Updating DB size file"
-$ZFS_BIN get -p -H -o value logicalused $TON_ZFS_FS > $TARGET_PATH/latest$NAME_SUFFIX.size.disk.txt
+$RM_BIN $TARGET_PATH/latest$NAME_SUFFIX.$DUMP_TYPE.*
+$LN_BIN -s $TARGET_PATH/$ARCHIVE_NAME.lz $TARGET_PATH/latest$NAME_SUFFIX.$DUMP_TYPE.lz
+$LN_BIN -s $TARGET_PATH/$ARCHIVE_NAME.sha256sum.txt $TARGET_PATH/latest$NAME_SUFFIX.$DUMP_TYPE.sha256sum.txt
+$LN_BIN -s $TARGET_PATH/$ARCHIVE_NAME.size.archive.txt $TARGET_PATH/latest$NAME_SUFFIX.$DUMP_TYPE.size.archive.txt
+$LN_BIN -s $TARGET_PATH/$ARCHIVE_NAME.size.disk.txt $TARGET_PATH/latest$NAME_SUFFIX.$DUMP_TYPE.size.disk.txt
 
 echo "Removing old archives"
-$FIND_BIN $TARGET_PATH/$ARCHIVE_PREFIX$NAME_SUFFIX* -type f -mtime +$ARCHIVE_LIFETIME -exec $RM_BIN {} \;
+$FIND_BIN $TARGET_PATH/$ARCHIVE_PREFIX$NAME_SUFFIX.* -type f -mtime +$ARCHIVE_LIFETIME -exec $RM_BIN {} \;
 
 echo "Mission acomplished!"
